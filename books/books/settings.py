d9244e3df5463cead890d559a123ca8ce6aea486
Amartya2503 <amartyamishra2503@gmail.com>
2023-05-15 18:35:52 +0530

    added .env file securing passwords

--- books/books/settings.py
+++ books/books/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xgq(6ycj=+^7nu*4w(f(c%ygxrahx94*+s8-&5v@8v&rhbs6q9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,9 +90,9 @@ DATABASES = {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'postgres',
        'USER': 'postgres',
-       'PASSWORD': 'pass@123amartya',
+       'PASSWORD': config('PASSWORD_DB'),
        'HOST': 'localhost',
-       'PORT': '5432',
+       'PORT': config('PORT'),
    }
 }
 
